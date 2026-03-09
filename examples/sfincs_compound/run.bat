c79fe4583b3c4e11130ee046aabe4a1fe26cf342
matti0208 <m.bellani@student.tudelft.nl>
2026-03-09 10:12:32 +0100

    Last tests

--- examples/sfincs_compound/run.bat
+++ examples/sfincs_compound/run.bat
@@ -1,1 +1,1 @@
-call "c:\put\your\own\path\sfincs.exe">sfincs_log.txt
+call "C:\Data\TUD\MSc_CE\Courses\2nd_year\7.CIE5060_Thesis\Modelling\Executables\SFINCS_2025_02_release\SFINCS_v2.3.0_mt_Faber_release_exe\sfincs.exe">sfincs_log.txt
